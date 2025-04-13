eab528f7f0de314b1b2549d8d84ab34e50eeb0af
D24125405 <elkhen.gal@gmail.com>
2025-04-13 08:31:33 +0100

    fixed shooting animation coding error

--- Player/player.gd
+++ Player/player.gd
@@ -7,38 +7,40 @@ const SPEED = 300
 const Jump = -300
 const Jump_horizontal = 100
 
-enum State { Idle, Run, Jump }
+enum State { Idle, Run, Jump, Shoot }
 
 var current_state : State
-
+var shoot_timer := 0.0  
 
 func _ready():
 	current_state = State.Idle
 
-	
-	
 func _physics_process(delta):
-	player_falling(delta)
-	player_Idle(delta)
-	player_run(delta)
-	player_jump(delta)
+	player_shooting(delta)
+
 	
+	if shoot_timer <= 0:
+		player_falling(delta)
+		player_Idle(delta)
+		player_run(delta)
+		player_jump(delta)
+	else:
+		shoot_timer -= delta
+		if shoot_timer <= 0:
+			current_state = State.Idle
+
 	move_and_slide()
-	
 	player_animations()
-	
 	print("State: ", State.keys()[current_state])
-	
+
 func player_falling(delta):
 	if !is_on_floor():
 		velocity.y += GRAVITY * delta 
-		
-		
+
 func player_Idle(delta):
 	if is_on_floor():
 		current_state = State.Idle
-		
-		
+
 func player_run(delta):
 	var direction = Input.get_axis("move_left", "move_right")
 	
@@ -50,10 +52,7 @@ func player_run(delta):
 	if direction != 0:
 		current_state = State.Run
 		animated_sprite_2d.flip_h = false if direction > 0 else true
-	
-		
-		
-		
+
 func player_jump(delta):
 	if Input.is_action_just_pressed("Jump"):
 		velocity.y = Jump
@@ -62,14 +61,19 @@ func player_jump(delta):
 	if !is_on_floor() and current_state == State.Jump:
 		var direction = Input.get_axis("move_left", "move_right")
 		velocity.x += direction * Jump_horizontal * delta 
-	
-		
-		
+
+func player_shooting(delta):
+	if Input.is_action_just_pressed("shoot") and is_on_floor():
+		current_state = State.Shoot
+		shoot_timer = 0.3  
+
 func player_animations():
-	if current_state == State.Idle:
-		animated_sprite_2d.play("Idle")
-	elif current_state == State.Run:
-		animated_sprite_2d.play("Run")
-	elif current_state == State.Jump:
-		animated_sprite_2d.play("Jump")
-	
+	match current_state:
+		State.Idle:
+			animated_sprite_2d.play("Idle")
+		State.Run:
+			animated_sprite_2d.play("Run")
+		State.Jump:
+			animated_sprite_2d.play("Jump")
+		State.Shoot:
+			animated_sprite_2d.play("Shot1")
