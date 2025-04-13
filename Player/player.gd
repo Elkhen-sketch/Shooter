6f54a26a5acff7a9d1a09f82e0069e8a1ecbde21
D24125405 <elkhen.gal@gmail.com>
2025-04-13 08:43:45 +0100

    fixeed merging errror

--- Player/player.gd
+++ Player/player.gd
@@ -7,30 +7,18 @@ const SPEED = 300
 const Jump = -300
 const Jump_horizontal = 100
 
-<<<<<<< HEAD
 enum State { Idle, Run, Jump, Shoot }
-=======
-enum State { Idle, Run, Jump, Shot }
->>>>>>> 530f1b82dbf11fb30956c665d6d65b35706e49be
 
 var current_state : State
-var shoot_timer := 0.0  
+var shoot_timer := 0.0  # Timer to control duration of shoot animation
 
 func _ready():
 	current_state = State.Idle
 
 func _physics_process(delta):
-<<<<<<< HEAD
 	player_shooting(delta)
 
-=======
-	player_falling(delta)
-	player_Idle(delta)
-	player_run(delta)
-	player_jump(delta)
-	player_shooting(delta)
->>>>>>> 530f1b82dbf11fb30956c665d6d65b35706e49be
-	
+	# If currently shooting, skip other movement states
 	if shoot_timer <= 0:
 		player_falling(delta)
 		player_Idle(delta)
@@ -73,12 +61,11 @@ func player_jump(delta):
 	if !is_on_floor() and current_state == State.Jump:
 		var direction = Input.get_axis("move_left", "move_right")
 		velocity.x += direction * Jump_horizontal * delta 
-<<<<<<< HEAD
 
 func player_shooting(delta):
 	if Input.is_action_just_pressed("shoot") and is_on_floor():
 		current_state = State.Shoot
-		shoot_timer = 0.3  
+		shoot_timer = 0.3  # Shooting lasts 0.3 seconds
 
 func player_animations():
 	match current_state:
@@ -90,23 +77,3 @@ func player_animations():
 			animated_sprite_2d.play("Jump")
 		State.Shoot:
 			animated_sprite_2d.play("Shot1")
-=======
-	
-	
-func player_shooting(delta: float):
-	var direction = Input.get_axis("move_left", "move_right")
-	
-	if direction != 0 and Input.is_action_pressed("Shot"):
-		current_state = State.Shot
-
-func player_animations():
-	if current_state == State.Idle:
-		animated_sprite_2d.play("Idle")
-	elif current_state == State.Run:
-		animated_sprite_2d.play("Run")
-	elif current_state == State.Jump:
-		animated_sprite_2d.play("Jump")
-	elif current_state == State.Shot:
-		animated_sprite_2d.play("shot")
-	
->>>>>>> 530f1b82dbf11fb30956c665d6d65b35706e49be
